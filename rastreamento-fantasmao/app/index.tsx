ce13acea907c3915d0416861fa5363c58144e242
Angelus Rocha <angelus_rocha@ufpi.edu.br>
2025-11-20 16:02:26 -0300

    Mapa Inicial adicionado

--- rastreamento-fantasmao/app/index.tsx
+++ rastreamento-fantasmao/app/index.tsx
@@ -1,15 +1,44 @@
-import { Text, View } from "react-native";
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
-export default function Index() {
+export default function App() {
+  // Ponto inicial do mapa (Exemplo genérico).
+  // Depois vamos mudar para a latitude/longitude da sua universidade.
+  const regiaoInicial = {
+    latitude: -22.9068, // Exemplo: Rio de Janeiro
+    longitude: -43.1729,
+    latitudeDelta: 0.01, // Define o nível de zoom (quanto menor, mais zoom)
+    longitudeDelta: 0.01,
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      {/* O Componente MapView renderiza o mapa do Google/Apple */}
+      <MapView 
+        style={styles.map} 
+        initialRegion={regiaoInicial}
+      >
+        {/* Vamos colocar um marcador fixo só para testar */}
+        <Marker
+          coordinate={{
+            latitude: -22.9068,
+            longitude: -43.1729,
+          }}
+          title="O Fantasmão"
+          description="Localização atual simulada"
+        />
+      </MapView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
